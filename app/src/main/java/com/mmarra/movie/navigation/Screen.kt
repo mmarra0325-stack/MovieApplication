8b908c06e704aa5a693fa606c79e022448a66d76
Danila <mmarra0325@gmail.com>
2025-12-01 10:34:50 +0500

    Добавила ресурсы и начала навигацию

--- app/src/main/java/com/mmarra/movie/navigation/Screen.kt
+++ app/src/main/java/com/mmarra/movie/navigation/Screen.kt
@@ -15,6 +15,12 @@ sealed class Screen(val route: String) {
     data object FavoriteMovies : Screen("favorite_movies")
 
     @Serializable
+    data object Profile : Screen("profile")
+
+    @Serializable
+    data object ProfileEdit : Screen("profile_edit")
+
+    @Serializable
     data class MovieDetail(val id: Int) : Screen("movie_detail/$id") {
         companion object {
             const val routeWithArg = "movie_detail/{id}"
